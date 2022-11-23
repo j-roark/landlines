7850ee7537a72fc8c6de20ab9b10be9d709624a8
j-roark <65353891+j-roark@users.noreply.github.com>
2022-11-22 20:44:49 -0500

    add cleanup; add "BeginDialToPeer"

--- sv_hooks.lua
+++ sv_hooks.lua
@@ -1,18 +1,43 @@
-util.AddNetworkString("StartDial")
+util.AddNetworkString("BeginDialToPeer")
+util.AddNetworkString("ixConnectedCallStatusChange")
 
-net.Receive("StartDial", function ()
+net.Receive("BeginDialToPeer", function (len, client)
     local dialSeq = net.ReadString()
-    if (string.len(dialSeq) != 4 or tonumber(dialSeq) == nil) then
+    if (string.len(dialSeq) < 3 or string.len(dialSeq) > 4 or tonumber(dialSeq) == nil) then
         return
     end
+
+    local exchange = net.ReadInt(5)
+    local extension = net.ReadInt(11)
+    local character = client:GetCharacter()
+    local vars = character:GetLandlineConnection()
+    
+    -- verify that this is coming from the correct place (or that no one is trying to manually call the hook)
+    if (vars["exchange"] != exchange and vars["extension"] != extension) then
+        return
+    end
+    
+    ix.phone.switch:Dial(exchange, extension, function(status)
+        character:SetLandlineConnection({
+            active = status,
+            exchange = exchange,
+            extension = extension
+        })
+    end, dialSeq)
 end)
 
 -- PHONE CLEANUP
 local function cleanupActivePhoneConnectionsForClient(client)
     local char = client:GetCharacter()
-    if (IsValid(char) and char:GetVar("landlineConnection")["active"]) then
+    if (char:GetLandlineConnection()["active"]) then
         ix.phone.switch:DisconnectActiveCallIfPresentOnClient(char)
     end
+
+    char:SetLandlineConnection({
+        active = false,
+        exchange = nil,
+        extension = nil
+    })
 end
 
 function PLUGIN:PlayerDisconnected(client)
